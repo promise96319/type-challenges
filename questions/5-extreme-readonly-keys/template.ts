93089a68cfee483c9df21a575c2cf64faa25e296
promise96319 <1248975357@qq.com>
2022-04-18 12:56:19 +0800

    feat: GetReadonlyKeys

--- questions/5-extreme-readonly-keys/template.ts
+++ questions/5-extreme-readonly-keys/template.ts
@@ -1,1 +1,7 @@
-type GetReadonlyKeys<T> = any
+// 判断类型相等 https://github.com/microsoft/TypeScript/issues/27024
+type MyEquals<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false
+
+// 通过 MyEquals 判断是否是 readonly
+type GetReadonlyKeys<T> = keyof {
+  [K in keyof T as MyEquals<Pick<T, K>, Readonly<Pick<T, K>>> extends true ? K : never]: any
+}
