a46a42938d09214d0406142161df0e94a42f14a7
promise96319 <1248975357@qq.com>
2022-04-18 13:14:21 +0800

    feat: DeepReadonly

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,1 +1,11 @@
-type DeepReadonly<T> = any
+// https://github.com/type-challenges/type-challenges/issues/8808
+// is object but not function
+type Enumerable<T> = T extends object
+  ? T extends Function
+    ? false
+    : true
+  : false
+
+type DeepReadonly<T> = {
+  readonly [P in keyof T]: Enumerable<T[P]> extends true ? DeepReadonly<T[P]> : T[P]
+}
