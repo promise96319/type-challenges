0bbe5cb38a84d96bf96713cee9ed2b1eef44e1a1
promise96319 <1248975357@qq.com>
2022-05-17 22:55:47 +0800

    feat: 55

--- questions/55-hard-union-to-intersection/template.ts
+++ questions/55-hard-union-to-intersection/template.ts
@@ -1,1 +1,8 @@
-type UnionToIntersection<U> = any
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#type-inference-in-conditional-types
+// type Bar<T> = T extends { a: (x: infer U) => void; b: (x: infer U) => void }
+//   ? U
+//   : never;
+// type T20 = Bar<{ a: (x: string) => void; b: (x: string) => void }>; // string
+// type T21 = Bar<{ a: (x: string) => void; b: (x: number) => void }>; // string & number
+// 逆变与协变
+type UnionToIntersection<U> = (U extends any ? (args: U) => any : never) extends (args: infer I) => any ? I : never
