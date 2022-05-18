387f2f262ae036d22eb945a9e3bd5b45c6e2e814
promise96319 <1248975357@qq.com>
2022-05-18 23:22:30 +0800

    feat: c-printf-parser

--- questions/147-hard-c-printf-parser/template.ts
+++ questions/147-hard-c-printf-parser/template.ts
@@ -8,4 +8,9 @@ type ControlsMap = {
   p: 'pointer'
 }
 
-type ParsePrintFormat = any
+type ParsePrintFormat<S extends string, Result extends any[] = []> =
+  S extends `${any}%${infer Symbol}${infer R}`
+    ? Symbol extends keyof ControlsMap
+      ? ParsePrintFormat<R, [...Result, ControlsMap[Symbol]]>
+      : ParsePrintFormat<R, Result>
+    : Result
