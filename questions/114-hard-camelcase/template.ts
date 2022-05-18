047b26a7385285c247aa3af1b0f722d92413c8db
promise96319 <1248975357@qq.com>
2022-05-18 23:01:42 +0800

    feat: camelCase

--- questions/114-hard-camelcase/template.ts
+++ questions/114-hard-camelcase/template.ts
@@ -1,1 +1,8 @@
-type CamelCase<S extends string> = any
+type PickStringFirst<S extends string> = S extends `${infer F}${infer R}` ? F : ''
+type PickStringRest<S extends string> = S extends `${infer F}${infer R}` ? R : ''
+type MyUncapitalize<S extends string> = S extends `${infer F}${infer R}` ? `${Uncapitalize<F>}${R}` : S
+
+type MyCamelCase<S extends string> =
+  S extends `${infer F}_${infer R}`
+    ? MyUncapitalize<`${MyCapitalize<Lowercase<F>>}${MyCapitalize<MyCamelCase<R>>}`>
+    : Lowercase<S>
--- questions/114-hard-camelcase/test-cases.ts
+++ questions/114-hard-camelcase/test-cases.ts
@@ -1,10 +1,10 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
-  Expect<Equal<CamelCase<'FOOBAR'>, 'foobar'>>,
-  Expect<Equal<CamelCase<'foo_bar'>, 'fooBar'>>,
-  Expect<Equal<CamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
-  Expect<Equal<CamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
-  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<MyCamelCase<'foobar'>, 'foobar'>>,
+  Expect<Equal<MyCamelCase<'FOOBAR'>, 'foobar'>>,
+  Expect<Equal<MyCamelCase<'foo_bar'>, 'fooBar'>>,
+  Expect<Equal<MyCamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
+  Expect<Equal<MyCamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
+  Expect<Equal<MyCamelCase<''>, ''>>,
 ]
