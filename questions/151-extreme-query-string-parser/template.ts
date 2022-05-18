5f67bed2e6071798ae5b687fd273f891f70b43cf
promise96319 <1248975357@qq.com>
2022-05-19 00:17:33 +0800

    feat: string parser

--- questions/151-extreme-query-string-parser/template.ts
+++ questions/151-extreme-query-string-parser/template.ts
@@ -1,1 +1,30 @@
-type ParseQueryString = any
+type MyEqual<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+
+type ToObject<S extends string> =
+  S extends `${infer Key}=${infer Value}`
+    ? { [F in Key]: Value }
+    : { [F in S]: true }
+
+type MergeObject<O extends Record<string, any>, P extends Record<string, any>> = {
+  [K in keyof O | keyof P]:
+  K extends keyof O
+    ? K extends keyof P
+      ? P[K] extends [...infer I]
+        ? [...I, O[K]]
+        : MyEqual<P[K], O[K]> extends true
+          ? P[K]
+          : [P[K], O[K]]
+      : O[K]
+    : K extends keyof P
+      ? P[K]
+      : never
+}
+
+type ParseQueryString<S extends string, Res extends Record<string, any> = {}> =
+  S extends `${infer F}&${infer R}`
+    ? ParseQueryString<R, MergeObject<ToObject<F>, Res>>
+    : S extends ''
+      ? Res
+      : MergeObject<ToObject<S>, Res>
