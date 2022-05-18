4cefa95723c3e36290abb659218777897151b904
promise96319 <1248975357@qq.com>
2022-05-18 22:14:25 +0800

    feat: trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,4 @@
-type Trim<S extends string> = any
+type EmptyString = ' ' | '\n' | '\t'
+type TrimStringRight<S extends string> = S extends `${infer T}${EmptyString}` ? TrimStringRight<T> : S
+type TrimStringLeft<S extends string> = S extends `${EmptyString}${infer T}` ? TrimStringLeft<T> : S
+type Trim<S extends string> = TrimStringRight<TrimStringLeft<S>>
