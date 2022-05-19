a6b16595cb2f7064c4ac80d852b70178a8a9c1e8
promise96319 <1248975357@qq.com>
2022-05-19 23:42:04 +0800

    docs: flatter

--- questions/459-medium-flatten/template.ts
+++ questions/459-medium-flatten/template.ts
@@ -1,1 +1,7 @@
-type Flatten = any
+type Flatten<A extends any[], T extends any[] = []> = A extends [infer F, ...infer R]
+  ? F extends any[]
+    ? Flatten<R, Flatten<F, T>>
+    : [...T, F, ...Flatten<R>]
+  : T
+
+  type y = Flatten<[1, 2, [3, 4], [[[5]]]]>
