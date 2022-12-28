003bff0e74752245366b1d6b5fffbcf528f3208b
promise96319 <1248975357@qq.com>
2022-12-28 10:31:15 +0800

    feat: 8640

--- questions/1367-medium-remove-index-signature/template.ts
+++ questions/1367-medium-remove-index-signature/template.ts
@@ -1,1 +1,6 @@
-type RemoveIndexSignature<T> = any
+// list types of index type parameters
+type IndexTypes = [string, number, symbol]
+// check if a type is a possible index type
+type IsIndexType<T, U=IndexTypes> = U extends [infer Start, ...infer End] ? Start extends T ? true : IsIndexType<T, End> : false
+
+type RemoveIndexSignature<T> = { [key in keyof T as (IsIndexType<key> extends true ? never : key)]: T[key] }
--- questions/8640-medium-number-range/template.ts
+++ questions/8640-medium-number-range/template.ts
@@ -1,1 +1,3 @@
-type NumberRange<L, H> = any
+type NumberToArray<T, A extends any[] = []> = A['length'] extends T ? A : NumberToArray<T, [...A, 1]>
+type NumberAddtionOne<N> = [...NumberToArray<N>, 1]['length']
+type NumberRange<L, H, Total extends any[] = []> = L extends H ? [...Total, L][number] : NumberRange<NumberAddtionOne<L>, H, [...Total, L]>
